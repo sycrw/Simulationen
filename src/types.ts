b99313a3d2cf318361d2e17c0c5a5d32b75fbd12
sycrw <timmartinkausemann@gmail.com>
2023-07-08 13:26:54 +0200

    types

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export enum Color {
+    Red = "Red",
+    Green = "Green",
+    Blue = "Blue",
+    Yellow = "Yellow"
+}
+ 
+export enum Action {
+    Skip = "Skip",
+    Reverse = "Reverse",
+    DrawTwo = "DrawTwo",
+    DrawFour = "DrawFour",
+    Wild = "Wild"
+}
